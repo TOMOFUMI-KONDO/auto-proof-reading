ca2978e95fd687baf572645629dd3b837a4f3d3d
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2019-12-31 21:43:57 +0900

    update: 校正条件を一括削除する処理の追加

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,4 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{{$title}}</title>
-<link rel="stylesheet" href="{{asset('css')}}/{{$css}}.css">
+<script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
+<link rel="stylesheet" href="{{ asset('css') }}/{{$css}}.css">
+<script src="{{ asset('js') }}/{{$js}}.js"></script>
