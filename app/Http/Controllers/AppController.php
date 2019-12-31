7f097e8ef5cbf9388f746eb737658cfb3790048e
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2019-12-31 21:43:42 +0900

    update: 任意の条件で構成を行う処理の追加

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,22 +6,40 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    const CONDITION_NUMBER = 10;
+
     public function index() {
         $data = [
-            'before' => '',
-            'after' => '',
+            'condition_number' => self::CONDITION_NUMBER,
+            'before_rep' => '校正前',
+            'after_rep' => '校正後',
         ];
         return view('app.index', $data);
     }
 
     public function post(Request $request) {
         $sentence = $request->sentence;
-        $before = str_replace('れる', '<span class="replaced">れる</span>', $sentence);
-        $after = str_replace('れる', '<span class="replaced">られる</span>', $sentence);
+        $before_rep = '';
+        $after_rep = '';
+        for($i = 0; $i < self::CONDITION_NUMBER; $i++) {
+            $before_str = $request->input("before_str" . $i);
+            $after_str = $request->input("after_str" . $i);
+            if($i === 0) {
+                $before_rep = str_replace($before_str, '<span class="replaced">' . $before_str . '</span>', $sentence);
+                $after_rep = str_replace($before_str, '<span class="replaced">' . $after_str . '</span>', $sentence);
+            }
+            else {
+                $before_rep = str_replace($before_str, '<span class="replaced">' . $before_str . '</span>', $before_rep);
+                $after_rep = str_replace($before_str, '<span class="replaced">' . $after_str . '</span>', $after_rep);
+            }
+        }
+
         $data = [
-            'before' => $before,
-            'after' => $after,
+            'condition_number' => self::CONDITION_NUMBER,
+            'before_rep' => $before_rep,
+            'after_rep' => $after_rep,
         ];
+        $request->flash();
         return view('app.index', $data);
     }
 }
--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -8,6 +8,9 @@
         @slot('css')
             app
         @endslot
+        @slot('js')
+            app
+        @endslot
     @endcomponent
 @endsection
 
@@ -20,13 +23,29 @@
 @endsection
 
 @section('content')
-    <p>校正する文章を入力してください。</p>
     <form method="POST" action="/">
         {{ csrf_field() }}
-        <label>
-            <textarea name="sentence" placeholder="ここに入力" cols="100px" rows="10px"></textarea>
-        </label>
+        <div id="sentence">
+            <p>校正する文章を入力してください。</p>
+            <label><textarea name="sentence" placeholder="ここに入力" cols="100px" rows="10px">{{ old('sentence') }}</textarea></label>
+        </div>
+        <div id="conditions">
+            <div class="description">
+                <p>校正したい文字を入力してください。</p>
+                <p id="erase" class="button">条件を全消去</p>
+                <p>校正前</p>
+                <p>校正後</p>
+            </div>
+            @for ($i = 0; $i < $condition_number; $i++)
+                <label><input type="text" name="before_str{{$i}}" value="{{ old('before_str' . $i) }}"></label>
+                <label><input type="text" name="after_str{{$i}}" value="{{ old('after_str' . $i) }}"></label>
+            @endfor
+        </div>
         <input type="submit" value="送信">
     </form>
-    <p>{!! $sentence !!}</p>
+
+    <p>ここに校正前の文章が出ます。</p>
+    <p id="before_rep">{!! nl2br($before_rep) !!}</p>
+    <p>ここに校正後の文章が出ます。</p>
+    <p id="after_rep">{!! nl2br($after_rep) !!}</p>
 @endsection
