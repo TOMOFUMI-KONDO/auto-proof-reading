3ece575057870339e7467951196bc8d6d09fc80e
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2020-01-01 09:24:10 +0900

    fix: ローカルとリモートで外部ファイルの読み込み方を場合
    app('env')からローカル・リモートの判断をし、ローカルではasset、リモートではsecure_assetでcssやjsを読み込むようにした。

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,5 +2,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{{$title}}</title>
 <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
-<link rel="stylesheet" href="{{ secure_asset('css') }}/{{$css}}.css">
-<script src="{{ secure_asset('js') }}/{{$js}}.js"></script>
+@if(app('env') === 'local')
+    <link rel="stylesheet" href="{{ asset('css') }}/{{$css}}.css">
+    <script src="{{ asset('js') }}/{{$js}}.js"></script>
+@endif
+@if(app('env') === 'production')
+    <link rel="stylesheet" href="{{ secure_asset('css') }}/{{$css}}.css">
+    <script src="{{ secure_asset('js') }}/{{$js}}.js"></script>
+@endif
