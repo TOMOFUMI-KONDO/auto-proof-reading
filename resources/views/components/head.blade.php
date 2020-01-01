57613c95c14e81a2854b3ce99f2d478ef225e170
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2020-01-01 13:05:34 +0900

    type="text/javascript"の追加

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,9 +4,9 @@
 <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
 @if(app('env') === 'local')
     <link rel="stylesheet" href="{{ asset('css') }}/{{$css}}.css">
-    <script src="{{ asset('js') }}/{{$js}}.js"></script>
+    <script type="text/javascript" src="{{ asset('js') }}/{{$js}}.js"></script>
 @endif
 @if(app('env') === 'production')
     <link rel="stylesheet" href="{{ secure_asset('css') }}/{{$css}}.css">
-    <script src="{{ secure_asset('js') }}/{{$js}}.js"></script>
+    <script type="text/javascript" src="{{ secure_asset('js') }}/{{$js}}.js"></script>
 @endif
