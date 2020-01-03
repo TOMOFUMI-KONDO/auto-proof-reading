f13799e1d19c6723a763f1b6e6fec0bded37d955
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2020-01-03 15:09:00 +0900

    jQueryの読み込みバージョンの変更

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{{$title}}</title>
-<script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 @if(app('env') === 'local')
     <link rel="stylesheet" href="{{ asset('css') }}/{{$css}}.css">
     <script type="text/javascript" src="{{ asset('js') }}/{{$js}}.js"></script>
