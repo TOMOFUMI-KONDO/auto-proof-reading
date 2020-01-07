2054c8d11f4bce560001943fd35a4d898ee3760e
Tomofumi Kondo <ugax2kontomo0314@gmail.com>
2020-01-07 22:51:04 +0900

    _ide_helperの作成

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.9.0 on 2019-12-31 00:32:39.
+ * Generated for Laravel 6.9.0 on 2020-01-07 11:27:52.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
