e6b44b61cb853245026057ded0006638b26b8dc6
TOMOFUMI KONDO <ugax2kontomo0314@gmai.com>
2020-03-30 09:20:37 +0900

    add: docxファイルをダウンロードできるように

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -35,10 +35,8 @@ class AppController extends Controller
     }
 
     /**
-     * フォームの内容のバリデーションはAppRequestで行う。
      * @param AppRequest $request
      * @return Factory|View
-     * @throws \Illuminate\Validation\ValidationException
      */
     //フォームの内容のバリデーションはAppRequestで行う。
     public function post(AppRequest $request) {
